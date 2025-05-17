c2df5c291d181d06e8cfdb38dacc267097f6d3df
isla.cruanes <islacrur@gmail.com>
2025-05-17 13:05:41 +0200

    Test4 incrementar, decrementar y DB

--- ejercicios/04-setup-teardown/funciones.test.js
+++ ejercicios/04-setup-teardown/funciones.test.js
@@ -3,29 +3,55 @@ import { Contador, DB } from './funciones.js'
 
 describe('Setup y Teardown', () => {
   describe('Contador con beforeEach y afterEach', () => {
-
+      //Arrange
+    let contador
+    beforeEach(()=>{
+      contador = new Contador(0)
+    })
+  afterEach(()=> {
+    contador.reiniciar()
+  })
     it('debe incrementar correctamente', () => {
-      
+      //Act
+      let nuevoValor = contador.incrementar()
+      //Assert
+      expect(nuevoValor).toBe(1)
     })
 
+
     it('debe decrementar correctamente', () => {
-      
+        //Act
+      let nuevoValor = contador.decrementar()
+      //Assert
+      expect(nuevoValor).toBe(-1)
     })
 
     it('debe iniciar con el valor correcto en cada test', () => {
-      
+      let valor = contador.getValor()
+
+      expect(valor).toBe(0)
     })
   })
 
   describe('DB con beforeAll y afterAll', () => {
-   
-
-    it('debe insertar datos correctamente', () => {
-      
+     let baseDatos = new DB()
+    beforeAll(()=>{
+     baseDatos.conectar()
+    })
+    afterAll(()=> {
+      baseDatos.desconectar()
     })
 
-    it('debe obtener todos los datos', () => {
-      
+    it('debe insertar datos correctamente', () => {
+      let dato = 72
+      let cantidadDatos = baseDatos.insertar(dato)
+      expect(cantidadDatos).toBe(1)
+    })
+
+    it.only('debe obtener todos los datos', () => {
+      let miBaseDatos = [45, 334, 78, 1]
+      baseDatos.datos = [...miBaseDatos]
+      expect(baseDatos.obtenerTodos()).toStrictEqual(miBaseDatos)
     })
   })
 }) 
