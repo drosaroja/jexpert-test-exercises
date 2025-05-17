eeded822ffce7120240c3f6616f6514b2081bf2d
isla.cruanes <islacrur@gmail.com>
2025-05-17 12:03:11 +0200

    Test3: crear usuario, comprobar nombre, edad, activo y fecha

--- ejercicios/01-aserciones-basicas/funciones.test.js
+++ ejercicios/01-aserciones-basicas/funciones.test.js
@@ -45,36 +45,57 @@ describe('Aserciones básicas', () => {
     })
     
     it('debe asignar el nombre correctamente', () => {
-      
+      const nombre = 'Luna'
+      const edad = 38
+      const usuario = crearUsuario(nombre, edad)
+
+      expect(usuario.nombre).toBe(nombre)
     })
     
     it('debe asignar la edad correctamente', () => {
-      
+        const nombre = 'Luna'
+      const edad = 38
+      const usuario = crearUsuario(nombre, edad)
+
+      expect(usuario.edad).toBe(edad)
     })
     
     it('debe tener la propiedad activo como true', () => {
-      
+      const nombre = 'Luna'
+      const edad = 38
+      const usuario = crearUsuario(nombre, edad)
+
+      expect(usuario.activo).toBeTruthy()      
     })
     
     it('debe incluir una propiedad fecha con una instancia de Date', () => {
+      const nombre = 'Luna'
+      const edad = 38
+      const usuario = crearUsuario(nombre, edad)
+
+      expect(usuario.fecha).toBeInstanceOf(Date)     
       
     })
   })
   
   describe('Función obtenerTipo', () => {
     it('debe retornar "number" para números', () => {
+      expect(false).toBe(true)
       
     })
     
     it('debe retornar "string" para cadenas de texto', () => {
+      expect(false).toBe(true)
       
     })
     
     it('debe retornar "boolean" para valores booleanos', () => {
+      expect(false).toBe(true)
       
     })
     
     it('debe retornar "object" para objetos', () => {
+      expect(false).toBe(true)
       
     })
   })
