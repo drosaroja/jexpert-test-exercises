2239d202100523dde4e2b5d820f0027d9ceb4386
isla.cruanes <islacrur@gmail.com>
2025-05-20 17:32:47 +0200

    Mockeo: se usa spyOn

--- ejercicios/06-mocking-funciones/funciones.test.js
+++ ejercicios/06-mocking-funciones/funciones.test.js
@@ -3,12 +3,12 @@ import { ejecutarConCallback, ServicioExterno, Cliente } from './funciones.js'
 
 describe('Mocking de Funciones', () => {
   describe('vi.fn() - Mockear funciones', () => {
-    it.only('debe llamar al callback con el resultado correcto', () => {
+    it('debe llamar al callback con el resultado correcto', () => {
       const operacion = (arg1, arg2) => {
         return arg1 + arg2
       }
-    const a = 1
-    const b = 2
+      const a = 1
+      const b = 2
       const resultado = operacion(a, b)
 
       const callback = vi.fn()
@@ -20,8 +20,12 @@ describe('Mocking de Funciones', () => {
   })
 
   describe('vi.spyOn() - Espiar métodos', () => {
-    it('debe espiar el método llamarAPI del servicio', () => {
+    it.only('debe espiar el método llamarAPI del servicio', () => {
+      const servicio = new ServicioExterno()
+
+      const spy = vi.spyOn(servicio, 'llamarAPI')
       
+      expect(spy).toHaveBeenCalled()
     })
     
     it('debe mockear el método procesarResultado para devolver un valor personalizado', () => {
